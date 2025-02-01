65910b8c5acc495bc3d579cafec18e2da742c701
okomadelmas <okomaagnidelmas@gmail.com>
2025-02-01 18:47:04 -0500

    add feature

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,44 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0;
+    cart.forEach(element => {
+        total = element.quantity*element.cost;
+    });
+    return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+    handleContinueShopping();
   };
 
 
 
   const handleIncrement = (item) => {
+    item.quantity += 1;
+    dispatch(updateQuantity(item));
   };
 
   const handleDecrement = (item) => {
-   
+    item.quantity -= 1
+    if(item.quantity == 0)
+        dispatch(removeItem(item))
+    else
+        dispatch(updateQuantity(item));
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    let total = 0;
+    total = item.quantity*item.cost
+  };
+
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
   };
 
   return (
